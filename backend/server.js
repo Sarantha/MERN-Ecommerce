ead121a509ae586002097940d1abf2f0ea40ea3d
Sarantha <ssmahadoowage@gmail.com>
2023-06-08 00:29:30 +0530

    mongo db connection and product controller

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import dotEnv from 'dotenv';
 dotEnv.config();
-import products from './data/products.js';
-
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
+
+connectDB();
+
 const app = express();
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));

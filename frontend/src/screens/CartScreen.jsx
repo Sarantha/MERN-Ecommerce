739bfae8b7bab9a23180eb50d95f2cc86402ee96
Sarantha <ssmahadoowage@gmail.com>
2023-06-21 21:09:26 +0530

    change dollar to lkr

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -46,7 +46,7 @@ const CartScreen = () => {
                                         <Col md={3}>
                                             <Link to={`/product/${item._id}`}>{item.name}</Link>
                                         </Col>
-                                        <Col md={2}>${item.price}</Col>
+                                        <Col md={2}>Rs.{item.price}</Col>
                                         <Col md={2}>
                                             <Form.Control
                                                 as='select'
@@ -85,7 +85,7 @@ const CartScreen = () => {
                                     Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
                                     items
                                 </h2>
-                                $
+                                Rs.
                                 {cartItems
                                     .reduce((acc, item) => acc + item.qty * item.price, 0)
                                     .toFixed(2)}
